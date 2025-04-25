0b2fd699676ec358e49554cf0867752e925111a9
kavinthangavel <kxvinthxngxvel@gmail.com>
2025-04-25 12:18:19 +0530

    build: bump version to 2.0.1

--- simkl_mps/__init__.py
+++ simkl_mps/__init__.py
@@ -2,7 +2,7 @@
 Media Player Scrobbler for SIMKL package.
 """
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __author__ = "kavinthangavel"
 
 # Apply compatibility patches first, before any other imports
